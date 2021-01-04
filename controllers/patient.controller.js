d82c051752fc6052e33818f9a7a50db3384f0135
Ruhen-Bhuiyan <43878741+Ruhen-Bhuiyan@users.noreply.github.com>
2021-01-04 18:50:39 +0600

    Add files via upload

---
+++ controllers/patient.controller.js
@@ -0,0 +1,58 @@
+const PatientModel = require('../models/patient.model');
+
+exports.getPatientList = (req, res) => {
+    PatientModel.getAllPatients((err, patients) => {
+        console.log('Here');
+        if (err)
+            res.send(err);
+        console.log('Patients', patients);
+        res.send(patients);
+    })
+}
+
+exports.getPatientByID = (req, res) => {
+    PatientModel.getPatientByID(req.params.id, (err, patient) => {
+        if (err)
+            res.send(err);
+        console.log('Patient data', patient);
+        res.send(patient);
+
+    })
+}
+exports.createNewPatient = (req, res) => {
+  
+    const patientReqData = new PatientModel(req.body);
+    console.log('patientReqData', patientReqData);
+    if (req.body.constructor === Object && Object.keys(req.body).length === 0) {
+        res.send(400).send({ success: false, message: ' Fill all data' });
+    }
+    else {
+        PatientModel.createPatient(patientReqData, (err, patient) => {
+            if (err) 
+                res.send(err);
+                res.json({ status: true, message: ' Success ', data: patient.insertId })
+           
+        })
+    }
+}
+exports.updatePatient = (req,res) => {
+    const patientReqData = new PatientModel(req.body);
+    console.log('Update', patientReqData);
+    if (req.body.constructor === Object && Object.keys(req.body).length === 0) {
+        res.send(400).send({ success: false, message: 'Fill all fields' });
+    }
+    else {
+        PatientModel.updatePatient(req.params.id, patientReqData, (err, patient) => {
+            if (err)
+                res.send(err);
+            res.json({ status: true, message: ' Updated ' })
+        })
+    }
+}
+exports.deletePatient = (req, res) => {
+    PatientModel.deletePatient(req.params.id, (err, patient) => {
+        if (err)
+            res.send(err);
+        res.json({ success: true, message: 'Success' });
+    })
+}
---
+++ models/patient.model.js
@@ -0,0 +1,68 @@
+var databaseConn = require('../database.config');
+var Patient = function(patient){
+    this.first_name = patient.first_name;
+    this.last_name = patient.last_name;
+    this.phone = patient.phone;
+    this.number_of_vaccine = patient.number_of_vaccine;
+}
+Patient.getAllPatients = (result) => {
+    databaseConn.query('SELECT * FROM patients', (err, res) => {
+        if (err) {
+            console.log('Error', err);
+            result(null, err);
+        }
+        else {
+            console.log('Succ');
+            result(null, res);
+        }
+    })
+}
+
+Patient.getPatientByID = (id, result) => {
+    databaseConn.query('SELECT * FROM patients WHERE id = ?', id, (err, res) => {
+        if(err) {
+            console.log('Error', err);
+            result(null, err);
+        }
+        else {
+            result(null, res);
+        }
+    })
+}
+Patient.createPatient = (patientReqData, result)=>{
+    databaseConn.query('INSERT INTO patients SET? ', patientReqData, (err, res) => {
+        if (err) {
+            console.log('Error');
+            result(null, err);
+        }
+        else {
+            console.log('Success');
+            result(null, res)
+        }
+    })
+}
+Patient.updatePatient = (id, patientReqData, result) => {
+    databaseConn.query("Update patients SET first_name=?,last_name=?,phone=?,number_of_vaccine=? WHERE id=?",[patientReqData.first_name, patientReqData.last_name,patientReqData.phone,patientReqData.number_of_vaccine, id], (err, res) => {
+        if (err) {
+            console.log('Error');
+            result(null, err);
+        }
+        else {
+            console.log('Updated successfully');
+            result(null, res);
+        }
+    })
+}
+
+Patient.deletePatient = (id, result) => {
+    databaseConn.query('DELETE FROM patients WHERE id = ?', [id], (err, res) => {
+        if (err) {
+            console.log('Error');
+            result(null, err);
+        }
+        else {
+            result(null, res);
+        }
+    })
+}
+module.exports = Patient;
