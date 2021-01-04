6e4277991fc24b64121dbdbe739ed2709fb09572
Ruhen-Bhuiyan <43878741+Ruhen-Bhuiyan@users.noreply.github.com>
2021-01-04 18:48:08 +0600

    Add files via upload

---
+++ database.config.js
@@ -0,0 +1,12 @@
+const mysql = require('mysql');
+const databaseConn = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'covid'
+});
+databaseConn.connect(function (error) {
+    if (error) throw error;
+    console.log('Success');
+})
+module.exports = databaseConn;
---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = process.env.PORT || 5000;
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.get('/', (req, res) => {
+	res.send('Hello World!');
+});
+
+const patientRoutes = require('./routes/patient.route');
+app.use('/api/vi/patient', patientRoutes);
+
+app.listen(port, () => {
+	console.log(`Example app listening at ${port}`);
+});
+
