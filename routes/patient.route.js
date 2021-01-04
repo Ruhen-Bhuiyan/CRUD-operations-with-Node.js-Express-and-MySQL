37087aa58a055312ff029358bef04b3d215b327c
Ruhen-Bhuiyan <43878741+Ruhen-Bhuiyan@users.noreply.github.com>
2021-01-04 18:50:03 +0600

    Add files via upload

---
+++ routes/patient.route.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const patientController = require('../controllers/patient.controller');
+router.get('/', patientController.getPatientList);
+router.get('/:id', patientController.getPatientByID);
+router.post('/', patientController.createNewPatient);
+router.put('/:id', patientController.updatePatient);
+router.delete('/:id', patientController.deletePatient);
+module.exports = router;
