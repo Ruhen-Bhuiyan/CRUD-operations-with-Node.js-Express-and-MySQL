a43badacacb324c5716f9fd989c5ee5ce7865165
Ruhen-Bhuiyan <43878741+Ruhen-Bhuiyan@users.noreply.github.com>
2021-01-27 20:40:19 +0600

    Update patient.route.js

--- routes/patient.route.js
+++ routes/patient.route.js
@@ -5,5 +5,6 @@ router.get('/', patientController.getPatientList);
 router.get('/:id', patientController.getPatientByID);
 router.post('/', patientController.createNewPatient);
 router.put('/:id', patientController.updatePatient);
+
 router.delete('/:id', patientController.deletePatient);
 module.exports = router;
