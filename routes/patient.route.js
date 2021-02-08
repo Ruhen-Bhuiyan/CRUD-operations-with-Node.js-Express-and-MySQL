4f5ebdf26b8e04eb47ff4c00adc9ad8e5463c620
Ruhen-Bhuiyan <43878741+Ruhen-Bhuiyan@users.noreply.github.com>
2021-02-08 16:03:17 +0600

    Update patient.route.js

--- routes/patient.route.js
+++ routes/patient.route.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const patientController = require('../controllers/patient.controller');
 router.get('/', patientController.getPatientList);
+
 router.get('/:id', patientController.getPatientByID);
 router.post('/', patientController.createNewPatient);
 router.put('/:id', patientController.updatePatient);
