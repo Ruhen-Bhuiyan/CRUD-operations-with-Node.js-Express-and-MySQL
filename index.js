84963b79c5c6f1ca5d8439dd7125684da22747f7
Ruhen-Bhuiyan <43878741+Ruhen-Bhuiyan@users.noreply.github.com>
2021-05-26 03:51:41 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-
 const port = process.env.PORT || 5000;
 app.use(bodyParser.urlencoded({ extended: false }));
-
 app.use(bodyParser.json());
-
 app.get('/', (req, res) => {
 	res.send('Hello World!');
 });
